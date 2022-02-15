87c04ea257407bc275dbbc07af5386816b88f192
yhirano <23518736+YuichiHirano@users.noreply.github.com>
2022-02-15 15:56:30 +0000

    add comment for debug of random value

--- src/hooks/useDice.tsx
+++ src/hooks/useDice.tsx
@@ -3,7 +3,9 @@ import { useState } from 'react';
 const useDice = (): [string, (t: string[]) => void] => {
   const [target, setTarget] = useState('???');
   const rollDice = (candidates: string[]) => {
-    setTarget(candidates[Math.floor(Math.random() * candidates.length)]);
+    const rand = Math.random();
+    console.log('dice used random value=', rand);
+    setTarget(candidates[Math.floor(rand * candidates.length)]);
   };
 
   return [target, rollDice];
